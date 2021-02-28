33bcfc83f5efd7fc7549daff0418196a910845f1
Ali Ahmadi <33722178+aliaria@users.noreply.github.com>
2021-02-28 19:57:22 +0430

    Add files via upload

---
+++ Data/AFPP.js
@@ -0,0 +1,24 @@
+var AFPP ={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "X": 63.880090, "Y": 34.371678, "OBJECTID": 1, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 63.880090122813286, 34.371678336017965 ] } },
+{ "type": "Feature", "properties": { "X": 64.482435, "Y": 33.906810, "OBJECTID": 2, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 64.482434878634351, 33.906810264313684 ] } },
+{ "type": "Feature", "properties": { "X": 62.166268, "Y": 33.333896, "OBJECTID": 3, "Name": null, "Type": "Diesel", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 62.16626753576702, 33.333895540746262 ] } },
+{ "type": "Feature", "properties": { "X": 65.177475, "Y": 32.322030, "OBJECTID": 4, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 65.17747543935414, 32.322029747674037 ] } },
+{ "type": "Feature", "properties": { "X": 69.342960, "Y": 33.719487, "OBJECTID": 5, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 69.342959876047416, 33.719487196780456 ] } },
+{ "type": "Feature", "properties": { "X": 69.676084, "Y": 33.850358, "OBJECTID": 6, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 69.676084417273216, 33.850357551865507 ] } },
+{ "type": "Feature", "properties": { "X": 70.815251, "Y": 34.326250, "OBJECTID": 7, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 70.815251372640432, 34.326249752836304 ] } },
+{ "type": "Feature", "properties": { "X": 69.693930, "Y": 35.037114, "OBJECTID": 8, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 69.693930373860368, 35.03711372899614 ] } },
+{ "type": "Feature", "properties": { "X": 70.187669, "Y": 35.759875, "OBJECTID": 9, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 70.187668533353659, 35.759875009284301 ] } },
+{ "type": "Feature", "properties": { "X": 68.771889, "Y": 35.691466, "OBJECTID": 10, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 68.771889234715999, 35.691465505007358 ] } },
+{ "type": "Feature", "properties": { "X": 69.065872, "Y": 36.686556, "OBJECTID": 11, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 69.065871642170379, 36.68655609807584 ] } },
+{ "type": "Feature", "properties": { "X": 71.377696, "Y": 35.094340, "OBJECTID": 12, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 71.377696468767155, 35.094339765631581 ] } },
+{ "type": "Feature", "properties": { "X": 66.826263, "Y": 36.719036, "OBJECTID": 13, "Name": "Sheberghan", "Type": "Thermal_Gas", "Capacity_M": 200 }, "geometry": { "type": "Point", "coordinates": [ 66.826263457445691, 36.719035740641409 ] } },
+{ "type": "Feature", "properties": { "X": 67.245049, "Y": 35.990921, "OBJECTID": 14, "Name": null, "Type": "Thermal", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 67.24504859442203, 35.990920673014671 ] } },
+{ "type": "Feature", "properties": { "X": 66.547867, "Y": 34.998685, "OBJECTID": 15, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 66.5478665192352, 34.998685432810895 ] } },
+{ "type": "Feature", "properties": { "X": 66.747741, "Y": 34.824985, "OBJECTID": 16, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 66.747741243251994, 34.824984780324868 ] } },
+{ "type": "Feature", "properties": { "X": 67.235531, "Y": 34.877333, "OBJECTID": 17, "Name": null, "Type": "Hydro", "Capacity_M": 0 }, "geometry": { "type": "Point", "coordinates": [ 67.235530749950726, 34.877332922307232 ] } }
+]
+}
